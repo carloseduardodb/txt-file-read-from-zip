f36e2795974f75619d2d43168b171ee3d0afb53e
Carlos Eduardo Dias Batista <carloseduardodiasbatista@gmail.com>
2023-05-26 14:29:06 -0300

    refactor: add comments

--- read-and-publish-zip-mem.ts
+++ read-and-publish-zip-mem.ts
@@ -3,17 +3,30 @@ import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class ReadAndPublishZipMem {
+  /**
+   * Parses a zip file containing JSON data line by line.
+   * @param fileData An object containing information about the zip file to parse.
+   * @param fileData.exchange The exchange used to route the data.
+   * @param fileData.routingKey The routing key used to route the data.
+   * @param fileData.volume The path to the zip file containing data to parse.
+   */
   async call(fileData: {
     exchange: string;
     routingKey: string;
     volume: string;
   }) {
+    // Load the zip file
     const zip = new AdmZip(fileData.volume);
     const zipEntries = zip.getEntries();
+
+    // Parse the first entry in the zip file
     const firstZipEntry = zipEntries[0];
     const zipEntryContent = firstZipEntry.getData().toString('utf8');
+
+    // Split the entry into individual lines
     const zipEntryLines = zipEntryContent.split(/\r?\n|\r|\n/g);
 
+    // Loop through each line and parse the JSON data
     for (let i = 0; i < zipEntryLines.length; i++) {
       try {
         if (!zipEntryLines[i]) {
@@ -22,6 +35,7 @@ export class ReadAndPublishZipMem {
         const jsonData: unknown = JSON.parse(zipEntryLines[i]);
         jsonData;
       } catch (error) {
+        // Log any errors that occur during parsing
         Logger.error(error.message);
       }
     }
--- read-and-publish-zip.ts
+++ read-and-publish-zip.ts
@@ -3,30 +3,50 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ReadAndPublishZip {
+  /**
+   * Asynchronously calls a function to read a zip file and print its contents to the console.
+   * @param fileData An object containing information about the zip file to read.
+   * @param fileData.exchange The exchange to read from.
+   * @param fileData.routingKey The routing key to use.
+   * @param fileData.volume The path to the zip file to read.
+   */
   async call(fileData: {
     exchange: string;
     routingKey: string;
     volume: string;
   }) {
+    // Open the zip file with lazy entries to avoid loading everything into memory at once.
     yauzl.open(fileData.volume, { lazyEntries: true }, (err, zipFile) => {
       if (err) {
+        // Throw an error if the file couldn't be opened.
         throw new Error(`Erro ao abrir o arquivo zip: ${err}`);
       }
+
+      // Read the first entry in the zip file.
       zipFile.readEntry();
+
+      // When a new entry is found in the zip file, check if it's a file or a directory.
       zipFile.on('entry', (entry) => {
         if (/\/$/.test(entry.fileName)) {
+          // If the entry is a directory, read the next entry.
           zipFile.readEntry();
         } else {
+          // If the entry is a file, open a read stream to it.
           zipFile.openReadStream(entry, (err, readStream) => {
             if (err) {
+              // Throw an error if the read stream couldn't be opened.
               throw new Error(
                 `Erro ao abrir o fluxo de leitura para o arquivo dentro do zip: ${err}`,
               );
             }
+
+            // When data is received from the read stream, print it to the console.
             readStream.on('data', (chunk) => {
               const line = chunk.toString('utf8');
-              console.log('line: ', line);
+              line;
             });
+
+            // When the read stream ends, read the next entry in the zip file.
             readStream.on('end', () => {
               zipFile.readEntry();
             });
@@ -34,6 +54,7 @@ export class ReadAndPublishZip {
         }
       });
 
+      // When there are no more entries in the zip file, close the zip file.
       zipFile.on('end', () => {
         zipFile.close();
       });
